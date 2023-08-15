d754365a27f4c725d005ba11e736459d8b757ddd
Viviane Martini <martini.viviane@gmail.com>
2023-08-15 11:40:50 -0300

    add js files

---
+++ script.js
@@ -0,0 +1,73 @@
+const activeClass = 'active'
+
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li')
+    const tabContent = document.querySelectorAll('.js-tabcontent section')
+
+    if(tabMenu.length && tabContent.length){
+        tabContent[0].classList.add(activeClass)
+
+    function activeTab(index) {
+        tabContent.forEach((section) => {
+            section.classList.remove(activeClass)
+        })
+        tabContent[index].classList.add(activeClass)
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index)
+        })
+    })
+    }
+}
+
+initTabNav()
+
+function initAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+
+    if(accordionList.length){
+        accordionList[0].classList.add(activeClass)
+        accordionList[0].nextElementSibling.classList.add(activeClass)
+    }
+
+    function activeAccordion(){
+        this.classList.toggle(activeClass)
+        this.nextElementSibling.classList.toggle(activeClass)
+    }
+    
+    accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion)
+    })
+}
+
+initAccordion()
+
+function initScrollSmooth(){
+    function scrollToSection(){
+        event.preventDefault()
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href)
+    
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    
+        //Forma alternativa
+    /*     const topo = section.offsetTop
+        window.scrollTo({
+            top: topo,
+            behavior: 'smooth'
+        }) */
+    }
+    
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+    
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection)
+    })
+}
+
+initScrollSmooth()
